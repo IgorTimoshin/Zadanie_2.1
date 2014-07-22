ffe473225ff37ac3a1f20ae64935337422e57f7d
IgorTimoshin <igorek_timoshin@mail.ru>
2014-07-22 13:20:30 +0600

    Задание 2.1

---
+++ Circle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Circle
+    {
+        // Создадим открытые поля
+        public double _xO, _yO;                 // Координаты центра окружности
+        public double _radius;                  // Радиус окружности
+        public double _length;                  // Длина окружности
+        public double _s;                       // Площадь круга
+
+        // В метод конструктор, при создании экземпляра класса, будем передавать координаты центра окружности и радиус
+        public Circle(double xO, double yO, double rad)
+        {
+            // Приравниваем значения полей с параметрами, переданными конструктору при создании
+            _xO = xO;
+            _yO = yO;
+            _radius = rad;
+            // Вычисляем длину окружности и площадь круга
+            _length = 2 * Math.PI * _radius;
+            _s = Math.PI * _radius * _radius;
+        }
+
+        // Непараметризированный метод определяющий, пересекаются ли границы окружности с осями координат
+        public bool Intersect()
+        {
+            if (((Math.Abs(_xO) - _radius) <= 0) || ((Math.Abs(_yO) - _radius) <= 0) ||
+                (((Math.Abs(_xO) - _radius) <= 0) && ((Math.Abs(_yO) - _radius) <= 0)))
+                return true;
+            else
+                return false;
+        }
+
+        // Параметризированный метод, вычисляющий длину отрезка, отсекаемого окружностью.
+        // Если прямая не пересекает окружность, то возвращается значение 0
+        public double Segment(double k, double b)
+        {
+            double d = Math.Pow((2 * k * b - 2 * _xO - 2 * _yO * k), 2) - (4 + 4 * k * k) * (b * b - _radius * _radius + _xO * _xO + _yO * _yO - 2 * _yO * b);
+            if (d < 0)
+                return 0;
+            else if (d > 0)
+            {
+                double x1 = (-(2 * k * b - 2 * _xO - 2 * _yO * k) - Math.Sqrt(d)) / (2 + 2 * k * k);
+                double x2 = (-(2 * k * b - 2 * _xO - 2 * _yO * k) + Math.Sqrt(d)) / (2 + 2 * k * k);
+
+                if (x1 == x2)
+                    return 0;
+                else
+                {
+                    double y1 = k * x1 + b;
+                    double y2 = k * x2 + b;
+
+                    double l = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+                    return l;
+                }
+            }
+            else
+                return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите координату центра окружности - X");
+            // Считываем значение, введённое пользователем
+            string s = Console.ReadLine();
+            // Преобразовываем его в тип double
+            double x = double.Parse(s);
+
+            Console.WriteLine("Введите координату центра окружности - Y");
+            s = Console.ReadLine();
+            double y = double.Parse(s);
+
+            Console.WriteLine("Введите радиус окружности");
+            s = Console.ReadLine();
+            double r = double.Parse(s);
+
+            // Создаём экземпляр класса Circle и передаём ему, ранее введённые параметры x, y, r
+            Circle circle = new Circle(x, y, r);
+
+            Console.WriteLine("Длина окружности = {0}", circle._length);
+            Console.WriteLine("Площадь круга = {0}", circle._s);
+
+            // Проверяем, пересекает ли окружность оси координат
+            if (circle.Intersect())
+                Console.WriteLine("Окружность пересекает одну или обе оси координат");
+            else
+                Console.WriteLine("Окружность не пересекает оси координат");
+
+            Console.WriteLine("Введите угловой коэффициент прямой (K)");
+            s = Console.ReadLine();
+            double k = double.Parse(s);
+
+            Console.WriteLine("Введите коэффициент b для уравнения прямой");
+            s = Console.ReadLine();
+            double b = double.Parse(s);
+
+            Console.WriteLine("Длина отрезка, отсечённого окружностью = {0}", circle.Segment(k, b));
+
+            // Эту строку пишем для того, чтобы программа не закрывалась пока пользователь не нажмёт любую клавишу
+            Console.ReadKey();
+        }
+    }
+}
